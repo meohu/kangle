6f1d0d18ff1aa7d9969384a37ba40c527025da08
尐酱油 <scncwqs@126.com>
2017-12-30 02:13:30 +0800

    Create ep2.sh

---
+++ ep2.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+KANGLE_VERSION=3.5.11
+killall -9 kangle
+rm -rf kangle-*
+rm -rf kangle-*
+wget https://raw.githubusercontent.com/moehu/kangle/master/kangle-$KANGLE_VERSION.tar.gz
+tar xzf kangle-$KANGLE_VERSION.tar.gz
+cd kangle-$KANGLE_VERSION
+./configure --prefix=/vhs/kangle --enable-vh-limit --enable-disk-cache --enable-ipv6 --enable-ssl --enable-http2 --enable-vh-limit 
+make
+make install
+service kangle start
