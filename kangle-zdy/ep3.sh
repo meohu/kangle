34207d47ebe800ebcda614dc1ffaa8a325b553de
尐酱油 <scncwqs@126.com>
2018-01-20 11:10:44 +0800

    Update ep3.sh

--- kangle-zdy/ep3.sh
+++ kangle-zdy/ep3.sh
@@ -3,7 +3,7 @@ KANGLE_VERSION=3.5.8
 killall -9 kangle
 rm -rf kangle-*
 rm -rf kangle-*
-wget https://raw.githubusercontent.com/moehu/kangle-zdy/master/kangle-$KANGLE_VERSION.tar.gz
+wget https://raw.githubusercontent.com/moehu/kangle/kangle-zdy/master/kangle-$KANGLE_VERSION.tar.gz
 tar xzf kangle-$KANGLE_VERSION.tar.gz
 cd kangle-$KANGLE_VERSION
 ./configure --prefix=/vhs/kangle --enable-vh-limit --enable-disk-cache --enable-ipv6 --enable-ssl --enable-http2 --enable-vh-limit 
